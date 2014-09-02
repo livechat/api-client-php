78a5461f85eeac6999ab96cec1bf10fe32752995
morus12 <g.wyszynski@livechatinc.com>
2014-09-02 11:33:38 +0200

    one tab identation

--- lib/models/Chats.php
+++ lib/models/Chats.php
@@ -1,36 +1,29 @@
 <?php
 
-class Chats extends Model
-{
+class Chats extends Model{
 
-  public function get($params=array())
-  {
-    $paramsString = $this->parseParams($params);
-    $url = 'chats';
-				$this->encodeParams($params);
-				$paramsString = $this->parseParams($params);
-    $url .= $paramsString != "" ? "?" . $paramsString : "";
-    return parent::get($url);
-  }
+	public function get( $params=array() ) {
+		$paramsString = $this->parseParams( $params );
+		$url = 'chats';
+		$this->encodeParams( $params );
+		$paramsString = $this->parseParams( $params );
+		$url .= $paramsString != "" ? "?" . $paramsString : "";
+		return parent::get( $url );
+	}
 
-  public function getSingleChat($chatId)
-  {
-    $url = 'chats/' . $chatId;
-    return parent::get($url);
-  }
+	public function getSingleChat( $chatId ) {
+		$url = 'chats/' . $chatId;
+		return parent::get( $url );
+	}
 
-  public function parseParams($params, $without = "")
-  {
-    $return = "";
-    foreach ($params as $keyParam => $valueParam)
-    {
-      if (trim($valueParam) != "" && $keyParam != $without)
-      {
-        $return != "" ? $return.="&" : '';
-        $return.=$keyParam . '=' . $valueParam;
-      }
-    }
-    return $return;
-  }
-
+	public function parseParams( $params, $without = "" ) {
+		$return = "";
+		foreach ( $params as $keyParam => $valueParam ) {
+			if ( trim( $valueParam ) != "" && $keyParam != $without ) {
+				$return != "" ? $return.="&" : '';
+				$return.=$keyParam . '=' . $valueParam;
+			}
+		}
+		return $return;
+	}
 }
