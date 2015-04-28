965a1c32bd0e64d9a90ee1869ba24accc508a4a2
Jake Haas <jake@jakehaas.com>
2015-04-28 01:38:52 -0400

    Fix formatting

--- lib/models/CannedResponses.php
+++ lib/models/CannedResponses.php
@@ -1,45 +1,39 @@
 <?php
 
-class CannedResponses extends Model
-{
+class CannedResponses extends Model {
 
-	public function get($group = 0) 
-	{
-		$group = (int)$group;
-		$url = 'canned_responses?group='.$group;
-		
-		return parent::get( $url );
-	}
-	
-	public function getSingleResponse($id)
-	{
-		$id = (int)$id;
-		$url = 'canned_responses/'.$id;
-		
-		return parent::get( $url );
-	}
-        
-	public function addNewResponse($text, $tags)
-	{
-		$url = 'canned_responses';
-		
-		return parent::post($url, array('text' => $text, 'tags' => $tags));
-	}
-	
-    public function updateResponse($id, $text, $tags)
-	{
-		$id = (int)$id;
-		$url = 'canned_responses/'.$id;
-		
-		return parent::put($url, array('text' => $text, 'tags' => $tags));
-	}
-	
-	public function deleteResponse($id)
-	{
-		$id = (int)$id;
-		$url = 'canned_responses/'.$id;
-		
-		return parent::delete($url);
-	}
+    public function get($group = 0) {
+        $group = (int) $group;
+        $url = 'canned_responses?group=' . $group;
+
+        return parent::get($url);
+    }
+
+    public function getSingleResponse($id) {
+        $id = (int) $id;
+        $url = 'canned_responses/' . $id;
+
+        return parent::get($url);
+    }
+
+    public function addNewResponse($text, $tags) {
+        $url = 'canned_responses';
+
+        return parent::post($url, array('text' => $text, 'tags' => $tags));
+    }
+
+    public function updateResponse($id, $text, $tags) {
+        $id = (int) $id;
+        $url = 'canned_responses/' . $id;
+
+        return parent::put($url, array('text' => $text, 'tags' => $tags));
+    }
+
+    public function deleteResponse($id) {
+        $id = (int) $id;
+        $url = 'canned_responses/' . $id;
+
+        return parent::delete($url);
+    }
 
 }
