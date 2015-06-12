9be8c6f25393035f8f43279e3d74302d9f3ef1b2
Toni Rudolf <toni.rudolf@gmail.com>
2015-06-12 10:48:17 +0200

    fixed agents using login instead of id (according to API)

--- lib/LiveChat/Models/Agents.php
+++ lib/LiveChat/Models/Agents.php
@@ -3,36 +3,30 @@ namespace LiveChat\Models;
 
 class Agents extends BaseModel
 {
-	public function get($id=null)
-	{
-		$url = 'agents';
-		if (is_null($id) === false)
-		{
-			$url .= '/'.$id;
-		}
+    public function get($login = null)
+    {
+        $url = 'agents' . (null !== $login ? '/'.$login : '');
 
-		return parent::get($url);
-	}
+        return parent::get($url);
+    }
 
-	public function add($vars)
-	{
-		$url = 'agents';
+    public function add($vars)
+    {
+        $url = 'agents';
 
-		return parent::post($url, $vars);
-	}
+        return parent::post($url, $vars);
+    }
 
-	public function update($id, $vars)
-	{
-		$id = (int)$id;
-		$url = 'agents/'.$id;
-		return parent::put($url, $vars);
-	}
+    public function update($login, $vars)
+    {
+        $url = 'agents/'.$login;
+        return parent::put($url, $vars);
+    }
 
-	public function delete($id)
-	{
-		$id = (int)$id;
-		$url = 'agents/'.$id;
+    public function delete($login)
+    {
+        $url = 'agents/'.$login;
 
-		return parent::delete($url);
-	}
+        return parent::delete($url);
+    }
 }
