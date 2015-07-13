75e5434d01999ec75fe1a8f9f40d68d293094dee
morus12 <g.wyszynski@livechatinc.com>
2015-07-13 11:51:14 +0200

    in some cases agent id is not integer

--- lib/models/Agents.php
+++ lib/models/Agents.php
@@ -16,20 +16,17 @@ class Agents extends Model
 	public function add($vars)
 	{
 		$url = 'agents';
-
 		return parent::post($url, $vars);
 	}
 
 	public function update($id, $vars)
 	{
-		$id = (int)$id;
 		$url = 'agents/'.$id;
 		return parent::put($url, $vars);
 	}
 
 	public function delete($id)
 	{
-		$id = (int)$id;
 		$url = 'agents/'.$id;
 
 		return parent::delete($url);
